a410f06fa5ba91cf9eacfeca2e854c2c9bcc33b8
OlegShcheglov <olegsheglov1@mail.ru>
2025-07-29 07:42:53 +0500

    lesson4

--- lesson4/test_string_utils.py
+++ lesson4/test_string_utils.py
@@ -7,37 +7,15 @@ def string_utils():
     return StringUtils()
 
 
-def test_capitalize(string_utils):
+def test_capitalize_positive(string_utils):
     assert string_utils.capitalize("oleg") == "Oleg"
     assert string_utils.capitalize("Oleg") == "Oleg"
     assert string_utils.capitalize("") == ""
 
 
-def test_trim(string_utils):
-    assert string_utils.trim("   oleg") == "oleg"
-    assert string_utils.trim("oleg") == "oleg"
-    assert string_utils.trim("   ") == ""
-
-
-def test_contains(string_utils):
-    assert string_utils.contains("Oleg", "O") is True
-    assert string_utils.contains("Oleg", "K") is False
-    assert string_utils.contains("", "S") is False
-
-
-def test_delete_symbol(string_utils):
-    assert string_utils.delete_symbol("Oleg", "O") == "leg"
-    assert string_utils.delete_symbol("Oleg", "Pro") == "Oleg"
-    assert string_utils.delete_symbol("Oleg", "Ol") == "eg"
-
-
 def test_capitalize_negative(string_utils):
     with pytest.raises(AttributeError):
         string_utils.capitalize(None)
-
-
-def test_delete_symbol_negative(string_utils):
-    assert string_utils.delete_symbol("Oleg", "X") == "Oleg"  # Попытка удалить несуществующий символ
 
 
 # Переписал тест на удаление пробелов с использованием параметризации
@@ -46,5 +24,36 @@ def test_delete_symbol_negative(string_utils):
     ("oleg", "oleg"),
     ("   ", "")
 ])
-def test_trim(string_utils, input_string, expected):
+def test_trim_positive(string_utils, input_string, expected):
     assert string_utils.trim(input_string) == expected
+
+
+def test_trim_negative(string_utils):
+    assert string_utils.trim("") == ""
+    with pytest.raises(AttributeError):
+        string_utils.trim(None)
+
+
+def test_contains_positive(string_utils):
+    assert string_utils.contains("Oleg", "O") is True
+    assert string_utils.contains("Oleg", "K") is False
+    assert string_utils.contains("", "S") is False
+
+
+def test_contains_negative(string_utils):
+    assert string_utils.contains("", "O") is False
+    assert string_utils.contains("Oleg", "") is True
+    with pytest.raises(TypeError):
+        string_utils.contains("Oleg", None)
+
+def test_delete_symbol_positive(string_utils):
+    assert string_utils.delete_symbol("Oleg", "O") == "leg"
+    assert string_utils.delete_symbol("Oleg", "Pro") == "Oleg"
+    assert string_utils.delete_symbol("Oleg", "Ol") == "eg"
+
+
+def test_delete_symbol_negative(string_utils):
+    assert string_utils.delete_symbol("Oleg", "X") == "Oleg"  # Попытка удалить несуществующий символ
+    assert string_utils.delete_symbol("", "O") == ""
+    with pytest.raises(TypeError):
+        string_utils.delete_symbol("Oleg", None)
